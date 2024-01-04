31e729b5231f32263e6b1d07bca2ea38aae7ca14
Matteo Miceli <matteo.miceli.5@gmail.com>
2024-01-04 10:30:23 +0100

    🦄 refactor(strategia): merge con feat/pos manuale

--- prolog-project/sudoku/strategia.pl
+++ prolog-project/sudoku/strategia.pl
@@ -1,53 +1,35 @@
 % Predicato Wrapper
 risolviSudoku(Cammino):-
     iniziale(SudokuIniziale), % Carica lo stato iniziale
-    risolvi(SudokuIniziale,Cammino,[]).
+    pos(1,1),
+    risolvi(SudokuIniziale,Cammino,[]),!.
 
+risolviSudoku(Cammino):-
+    iniziale(SudokuIniziale), % Carica lo stato iniziale
+    retractall(pos(_,_)),
+    assert(pos(1,1)),
+    risolvi(SudokuIniziale,Cammino,[]),!.
+    
+% Check stato finale
 risolvi(Sudoku,[],_):-
     finale(Sudoku).
 
 % prova visita in profondità
 % risolvi([Riga|RestoSudoku],[Az|ListaAzioni],Visitati):-
 risolvi(Sudoku,[_|_],_):-
+    % Prende tutte le azioni possibili sulla casella in esame
+    % findall(Azione,applicabile(Azione,Sudoku),ListaAzioniApplicabili),
+
     applicabile(Azione,Sudoku).
     % trasforma(Azione,SudokuNuovo)
     %\+member(Snuovo,Visitati),
     %risoluzione(NuovoSudoku,ListaAzioni,[S|Visitati])
-
-% risolvi(Sudoku,[_|_],_):-
-%     applicabile(assegna(Riga,NuovaColonna,_),Sudoku).
-
-    % applicabile(Az,[Riga|RestoSudoku]),
-    % Condizioni di start
-
-    % trasforma(Snuovo),
-    % risolvi(ListaAzioni,Visitati).
-
-% MODIFICHE DA RIVEDERE
-% % Predicato Wrapper
-% prova(Cammino):-
-%     iniziale(S0),
-%     pos(1,1),
-%     risolvi(S0,Cammino,[]),!.
-
-% prova(Cammino):-
-%     iniziale(S0),
-%     retractall(pos(_,_)),
-%     assert(pos(1,1)),
-%     risolvi(S0,Cammino,[]),!.
-
-% risolvi(S,[],_):-
-%     finale(S),!.
-
-% % prova visita in profondità
-% % risolvi([Riga|RestoSudoku],[Az|ListaAzioni],Visitati):-
-% risolvi(Sudoku,[_|_],_):-
-%     findall(Azione,applicabile(Azione,Sudoku),ListaAzioniApplicabili),
-%     write(ListaAzioniApplicabili),
-%     % TO-DO: strategia da applicare
-%     risolvi(Sudoku,[_|_],_).
-%     % trasforma()
-
+    
+    % in questo modo, risolvi va in loop
+    risolvi(Sudoku,[_|_],_).
+    
+    % else: in questo caso, esce dopo il primo scorrimento.
+    % risolvi(Sudoku,[],_).
 
 
 % NOTA: potrebbe essere utile, nella strategia di ricerca in ampiezza,
